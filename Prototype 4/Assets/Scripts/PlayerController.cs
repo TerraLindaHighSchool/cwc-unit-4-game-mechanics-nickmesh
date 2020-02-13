242ef3e1e86bedfd41d01bf87437e5b13fc6bc3f
nickmesh <59099009+nickmesh@users.noreply.github.com>
2020-02-13 14:24:46 -0800

    Tutorial 4.3.3 Complete

--- Prototype 4/Assets/Scripts/PlayerController.cs
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,8 @@ public class PlayerController : MonoBehaviour
     private GameObject focalPoint;
     private Rigidbody playerRb;
     public float speed = 5.0f;
-    
+    public bool hasPowerUp = false; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +21,14 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Powerup"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+        }
     }
 }
